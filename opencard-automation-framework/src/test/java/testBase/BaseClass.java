7f522efbba0361396e659fdd4d5712dc85d79135
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-12-05 16:17:01 +0100

    add testng.xml. Closes #6

--- opencard-automation-framework/src/test/java/testBase/BaseClass.java
+++ opencard-automation-framework/src/test/java/testBase/BaseClass.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -21,11 +22,17 @@ public class BaseClass {
 
         logger = LogManager.getLogger(this.getClass());
 
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //switch-off message on the top of the browser
+        chromeOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://tutorialsninja.com/demo/index.php?route=common/home");
         driver.manage().window().maximize();
+
+
     }
 
     @AfterClass
