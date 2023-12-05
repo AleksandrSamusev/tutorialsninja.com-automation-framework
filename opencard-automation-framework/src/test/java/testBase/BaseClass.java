f3f019734bd1d3aa38a1900cef5451e0b7187904
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-12-05 14:54:14 +0100

    update BaseClass and add logging to AccountRegisterTest. Closes #5

--- opencard-automation-framework/src/test/java/testBase/BaseClass.java
+++ opencard-automation-framework/src/test/java/testBase/BaseClass.java
@@ -2,6 +2,8 @@ package testBase;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -12,9 +14,13 @@ import java.time.Duration;
 public class BaseClass {
 
     public WebDriver driver;
+    public Logger logger;
 
     @BeforeClass
     public void setup() {
+
+        logger = LogManager.getLogger(this.getClass());
+
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
--- opencard-automation-framework/src/test/java/testCases/TC_001_AccountRegistrationTest.java
+++ opencard-automation-framework/src/test/java/testCases/TC_001_AccountRegistrationTest.java
@@ -10,12 +10,17 @@ public class TC_001_AccountRegistrationTest extends BaseClass {
 
     @Test
     void test_account_registration() {
+        logger.debug("application logs...........");
+        logger.info("***  Starting TC_001_AccountRegistrationTest ***");
         try {
             HomePage homePage = new HomePage(driver);
             homePage.clickMyAccount();
+            logger.info("clicked on Account link");
             homePage.clickRegister();
-
+            logger.info("clicked on Register link");
             AccountRegistrationPage registrationPage = new AccountRegistrationPage(driver);
+
+            logger.info("providing customers data");
             registrationPage.setFirstName(randomString().toUpperCase());
             registrationPage.setLastName(randomString().toUpperCase());
             registrationPage.setEmail(randomString() + "@gmail.com");
@@ -27,13 +32,15 @@ public class TC_001_AccountRegistrationTest extends BaseClass {
             registrationPage.setConfirmPassword(password);
             registrationPage.clickPrivacyPolicy();
             registrationPage.clickContinue();
-
+            logger.info("clicked on Continue");
             String confMsg = registrationPage.getConfirmationMessage();
-
-            Assert.assertEquals(confMsg, "Your Account Has Been Created!");
+            logger.info("validating expected message");
+            Assert.assertEquals(confMsg, "Your Account Has Been Created!",
+                    "test failed");
         } catch (Exception ex) {
+            logger.info("test failed");
             Assert.fail();
         }
+        logger.info("***  Finished TC_001_AccountRegistrationTest ***");
     }
-
 }
