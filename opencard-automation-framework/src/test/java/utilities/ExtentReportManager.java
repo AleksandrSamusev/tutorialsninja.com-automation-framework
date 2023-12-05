e031710ac6d5f1f6e5ffa96b84743645af06e3ac
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-12-05 17:28:06 +0100

    add ExtentReportManager class. Closes #10

---
+++ opencard-automation-framework/src/test/java/utilities/ExtentReportManager.java
@@ -0,0 +1,63 @@
+package utilities;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import testBase.BaseClass;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ExtentReportManager implements ITestListener {
+
+    public ExtentSparkReporter sparkReporter;
+    public ExtentReports extentReports;
+    public ExtentTest extentTest;
+
+    String repName;
+
+    public void onStart(ITestContext testContext) {
+        String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        repName = "Test-Report-" + timestamp + ".html";
+
+        sparkReporter = new ExtentSparkReporter(".\\reports\\" + repName); //specify the location of the report
+
+        sparkReporter.config().setDocumentTitle("tutorialsninja Automation Report"); //Title of report
+        sparkReporter.config().setReportName("tutorialsninja Functional Testing"); //Name of the report
+        sparkReporter.config().setTheme(Theme.DARK);
+
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(sparkReporter);
+        extentReports.setSystemInfo("Application", "tutorialsninja");
+        extentReports.setSystemInfo("Module", "Admin");
+        extentReports.setSystemInfo("Sub Module", "Customers");
+        extentReports.setSystemInfo("Operating System", System.getProperty("os.name"));
+        extentReports.setSystemInfo("User Name", System.getProperty("user.name"));
+        extentReports.setSystemInfo("Enviroment", "QA");
+    }
+
+    public void onTestSuccess(ITestResult result) {
+        extentTest = extentReports.createTest(result.getName());
+        extentTest.log(Status.PASS, "Test Passed");
+    }
+
+   /* public void onTestFailure(ITestResult result) {
+        extentTest = extentReports.createTest(result.getName());
+        extentTest.log(Status.FAIL, "Test Failed");
+        extentTest.log(Status.FAIL, result.getThrowable().getMessage());
+
+        try {
+            String imgPath = new BaseClass().captureScreen(result.getName());
+            extentTest.addScreenCaptureFromPath(imgPath);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }*/
+
+}
