7018b5098b9aa8e882ad1d999466ddf23b6a42b5
alex <alex.samusev.81@gmail.com>
2023-12-13 23:00:22 +0100

    add TC_RF_010_03 automation. Closes #6

---
+++ src/test/java/testCases/RegisterFunctionality_RF/TC_RF_010_03_AccountNotCreatedIfEmailIncorrectTest.java
@@ -0,0 +1,60 @@
+package testCases.RegisterFunctionality_RF;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.AccountRegistrationPage;
+import pageObjects.HomePage;
+import testBase.BaseClass;
+
+public class TC_RF_010_03_AccountNotCreatedIfEmailIncorrectTest extends BaseClass {
+    @Test(groups = {"Regression", "Master"})
+    public void test_whenEmailFormatIncorrect_thenAccountNotCreated() {
+        logger.info("");
+        logger.info("***  Starting TC_RF_010_03_AccountNotCreatedIfEmailIncorrectTest ***");
+        try {
+            HomePage homePage = new HomePage(driver);
+            logger.info("... create Home Page");
+            homePage.clickMyAccount();
+            logger.info("... click on 'My account' in navbar");
+            homePage.clickRegister();
+            logger.info("... click on 'Register'");
+            AccountRegistrationPage registrationPage = new AccountRegistrationPage(driver);
+            logger.info("... create Account Registration Page");
+
+            //generate and save users params
+            logger.info("... generating user data:");
+            String fName = randomString().toUpperCase();
+            logger.info("first name: " + fName);
+            String lName = randomString().toUpperCase();
+            logger.info("last name: " + lName);
+            String mail = randomString(); //email format - <someText>
+            logger.info("email: " + mail);
+            String phone = randomNumber();
+            logger.info("phone number: " + phone);
+            String password = randomAlphaNumeric();
+            logger.info("password: " + password + ", confirm password: " + password);
+
+            //set parameters to corresponding fields
+            logger.info("... setting user data to the corresponding fields");
+            registrationPage.setFirstName(fName);
+            registrationPage.setLastName(lName);
+            registrationPage.setEmail(mail);
+            registrationPage.setTelephone(phone);
+            registrationPage.setPassword(password);
+            registrationPage.setConfirmPassword(password);
+            registrationPage.clickPrivacyPolicy();
+            logger.info("... click on 'accept policy rules' ");
+
+            registrationPage.clickContinue();
+            logger.info("... click 'Continue' button");
+            String msg = registrationPage.getStandardEmailValidationMessage();
+            logger.info("... validation message - " + msg);
+            Assert.assertEquals(msg, "Please include an '@' in the email address. '" +
+                    mail + "' is missing an '@'.");
+        } catch (Exception ex) {
+            logger.info("!! TEST FAILED !!");
+            Assert.fail();
+        }
+        logger.info("***  Finished TC_RF_010_03_AccountNotCreatedIfEmailIncorrectTest ***");
+    }
+}
