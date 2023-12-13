885c07a425d490940fe0093211857a1f60779378
alex <alex.samusev.81@gmail.com>
2023-12-14 00:34:27 +0100

    add TC_RF_011_04 automation. Closes #10

---
+++ src/test/java/testCases/RegisterFunctionality_RF/TC_RF_011_4_AccountRegisterWithInvalidTelNumberTest.java
@@ -0,0 +1,59 @@
+package testCases.RegisterFunctionality_RF;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.AccountRegistrationPage;
+import pageObjects.HomePage;
+import testBase.BaseClass;
+
+public class TC_RF_011_4_AccountRegisterWithInvalidTelNumberTest extends BaseClass {
+    @Test(groups = {"Regression", "Master"})
+    public void test_whenEmailFormatIncorrect_thenAccountNotCreated() {
+        logger.info("");
+        logger.info("***  TC_RF_011_4_AccountRegisterWithInvalidTelNumberTest ***");
+        try {
+            HomePage homePage = new HomePage(driver);
+            logger.info("... create Home Page");
+            homePage.clickMyAccount();
+            logger.info("... click on 'My account' in navbar");
+            homePage.clickRegister();
+            logger.info("... click on 'Register'");
+            AccountRegistrationPage registrationPage = new AccountRegistrationPage(driver);
+            logger.info("... create Account Registration Page");
+
+            //generate and save users params
+            logger.info("... generating user data:");
+            String fName = randomString().toUpperCase();
+            logger.info("first name: " + fName);
+            String lName = randomString().toUpperCase();
+            logger.info("last name: " + lName);
+            String mail = randomString() + "@gmail.com";
+            logger.info("email: " + mail);
+            String phone = "12345678901234567890123456789012345678901234567890";
+            logger.info("phone number: " + phone);
+            String password = randomAlphaNumeric();
+            logger.info("password: " + password + ", confirm password: " + password);
+
+            //set parameters to corresponding fields
+            logger.info("... setting user data to the corresponding fields");
+            registrationPage.setFirstName(fName);
+            registrationPage.setLastName(lName);
+            registrationPage.setEmail(mail);
+            registrationPage.setTelephone(phone);
+            registrationPage.setPassword(password);
+            registrationPage.setConfirmPassword(password);
+            registrationPage.clickPrivacyPolicy();
+            logger.info("... click on 'accept policy rules' ");
+
+            registrationPage.clickContinue();
+            logger.info("... click 'Continue' button");
+            String msg = registrationPage.getPhoneValidationMessage();
+            logger.info("... validation message - " + msg);
+            Assert.assertEquals(msg, "Telephone must be between 3 and 32 characters!");
+        } catch (Exception ex) {
+            logger.info("!! TEST FAILED !!");
+            Assert.fail();
+        }
+        logger.info("***  TC_RF_011_4_AccountRegisterWithInvalidTelNumberTest ***");
+    }
+}
