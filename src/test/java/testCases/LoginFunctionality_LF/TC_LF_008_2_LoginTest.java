46986d4ae1b3eb4ca7369d2706744d8bdfefedbb
alex <alex.samusev.81@gmail.com>
2023-12-18 00:32:28 +0100

    add TC_LF_008_2_LoginTest automation. Closes #69

--- src/test/java/testCases/LoginFunctionality_LF/TC_LF_008_2_LoginTest.java
+++ src/test/java/testCases/LoginFunctionality_LF/TC_LF_008_2_LoginTest.java
@@ -1,15 +1,16 @@
 package testCases.LoginFunctionality_LF;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 import testBase.BaseClass;
 
-public class TC_LF_008_1_LoginTest extends BaseClass {
+public class TC_LF_008_2_LoginTest extends BaseClass {
     @Test(groups = {"Regression", "Master"})
-    public void test_checkIfEmailFieldHasProperPlaceholder() {
+    public void test_checkIfPasswordFieldHasProperPlaceholder() {
         logger.info("");
-        logger.info("***  START TC_LF_008_1_LoginTest ***");
+        logger.info("***  START TC_LF_008_2_LoginTest ***");
         try {
             HomePage homePage = new HomePage(driver);
             logger.info("... create Home Page");
@@ -21,14 +22,14 @@ public class TC_LF_008_1_LoginTest extends BaseClass {
             LoginPage loginPage = new LoginPage(driver);
             logger.info("... create 'Login' Page");
 
-            String msg = loginPage.getTxtEmail().getAttribute("placeholder");
+            String msg = loginPage.getTxtPassword().getAttribute("placeholder");
             logger.info("... check the title on the My Account page");
-            Assert.assertEquals(msg, "E-Mail Address", "Placeholder not match !! TEST FAILED !!");
+            Assert.assertEquals(msg, "Password", "Placeholder not match !! TEST FAILED !!");
 
         } catch (Exception ex) {
             logger.info("!! TEST FAILED !!");
             Assert.fail();
         }
-        logger.info("***  END TC_LF_008_1_LoginTest ***");
+        logger.info("***  END TC_LF_008_2_LoginTest ***");
     }
 }
